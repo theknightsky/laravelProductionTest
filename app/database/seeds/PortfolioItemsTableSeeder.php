cb4da6207bdb82c12bacba7ac5857919d3ee2699
Skyler Knight <skyler.knight22@gmail.com>
2015-03-19 01:08:37 +0900

    added item for db:seed test

--- app/database/seeds/PortfolioItemsTableSeeder.php
+++ app/database/seeds/PortfolioItemsTableSeeder.php
@@ -7,16 +7,22 @@ class PortfolioItemsTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
+		// $faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
-		{
-			PortfolioItem::create([
-                'title' => "Passkeep",
-                'description' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi itaque ut exercitationem dolorem placeat, reprehenderit rerum minus ullam, tenetur assumenda earum eos a, omnis odio possimus est recusandae eum similique.",
-                'resources' => "html,css,js",
-			]);
-		}
+		// foreach(range(1, 10) as $index)
+		// {
+		// 	PortfolioItem::create([
+  //               'title' => "Passkeep",
+  //               'description' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi itaque ut exercitationem dolorem placeat, reprehenderit rerum minus ullam, tenetur assumenda earum eos a, omnis odio possimus est recusandae eum similique.",
+  //               'resources' => "html,css,js",
+		// 	]);
+		// }
+
+        portfolioItem::create(array(
+            'title' => 'Passkeep',
+            'description' => '  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt neque nisi nobis nemo, fugit cupiditate tempora incidunt magnam aperiam quidem consequatur, in repellendus molestiae vel est accusamus aut aspernatur, commodi!',
+            'resources' => 'html,css,sass,js,angularjs',
+        ));
 	}
 
 }
